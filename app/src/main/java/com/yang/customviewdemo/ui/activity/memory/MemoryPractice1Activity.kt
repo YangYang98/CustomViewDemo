2056053cec68ec7c6525916146afb40bda89d860
yangyang <yangyang@yax.xyz>
2023-12-26 20:49:28 +0800

    MOD：内存优化 ---> 简单实现一个计时器 ---> 解决循环中创建大量数组造成的内存抖动问题

--- app/src/main/java/com/yang/customviewdemo/ui/activity/memory/MemoryPractice1Activity.kt
+++ app/src/main/java/com/yang/customviewdemo/ui/activity/memory/MemoryPractice1Activity.kt
@@ -18,6 +18,8 @@ class MemoryPractice1Activity : AppCompatActivity() {
 
     private var count = 0
 
+    private val array = IntArray(1000)
+
     private val handler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
@@ -27,7 +29,6 @@ class MemoryPractice1Activity : AppCompatActivity() {
 
             //模拟一些复杂的业务逻辑，创建大量的数组对象
             for (i in 0 until 100) {
-                val array = IntArray(1000)
                 array[0] = i
             }
 
